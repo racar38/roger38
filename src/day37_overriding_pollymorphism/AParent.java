3e9a5e56f299838baa097d4933b6d831de5449a7
RacaRgit config user.name <acar-resul-38@hotmail.com>
2023-08-16 01:43:59 +0300

    20230816

---
+++ src/day37_overriding_pollymorphism/AParent.java
@@ -0,0 +1,15 @@
+package day37_overriding_pollymorphism;
+
+public class AParent {
+
+    public final void finalMethod(){
+        System.out.println("Bu bir final method");
+    }
+    public static void staticMethod(){
+        System.out.println("Bu bir static method");
+    }
+    private void privateMethod(){
+        System.out.println("Bu bir private method");
+    }
+
+}
---
+++ src/day37_overriding_pollymorphism/C_MecburiOlmayanParent.java
@@ -0,0 +1,16 @@
+package day37_overriding_pollymorphism;
+
+public class C_MecburiOlmayanParent {
+    public void method1(){
+        System.out.println("parent method 1");
+    }
+    public void method2(){
+        System.out.println("parent method 2");
+    }
+    public void method3(){
+        System.out.println("parent method 3");
+    }
+    public void method4(){
+        System.out.println("parent method 4");
+    }
+}
